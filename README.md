5eb7bb1327a0891bd9d3ac4c1885cf0323738482
Matt Lee <crazoter@gmail.com>
2020-06-21 00:18:11 +0800

    Update gen_kaguya.cmd to output png, embedded schema into readme

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # kaguya-sama-sqlite3
 A SQLite3 database on the Kaguya-sama: Love is War manga that I update during my free time for fun
 
+Updated to: Chapter 0 (Incomplete Schema)
+
 Files
 ---
 
 * kaguya_data.db:  SQLite3 file containing currently stored information on the manga series.
-    * Currently updated to: Chapter 0 (Schema is not ready!)
 * kaguya_data.sqbpro: [DB Browser for SQLite](https://sqlitebrowser.org/) project file
 * schema.pdf: Diagram of the database schema
 * gen_diagram_helper.cmd: Helper windows command script to generate schema.pdf using SchemaCrawler
@@ -14,3 +15,8 @@ Tools used
 ---
 * [DB Browser for SQLite](https://sqlitebrowser.org/)
 * [SchemaCrawler](https://www.schemacrawler.com/diagramming.html) for diagramming the schema
+
+Schema
+---
+![Image of schema](./schema.png)
+
--- gen_kaguya.cmd
+++ gen_kaguya.cmd
@@ -1,10 +1,10 @@
 REM This is a helper cmd script to generate the schema diagram using schemacrawler.
-REM Modify the REPO_PATH variable below and place this in the "schemacrawler-ver-distribution\examples\diagram" directory.
+REM Modify the REPO_PATH variable below and place this script in the "schemacrawler-ver-distribution\examples\diagram" directory.
 
 SETLOCAL
-REM Remark: Modify the repo_path value to point to the repository directory e.g. "D:\path\kaguya-sama"
-SET REPO_PATH="YOUR_PATH_HERE\kaguya-sama"
+REM Remark: Modify the repo_path value to point to the repository directory e.g. "D:\...\kaguya-sama"
+SET REPO_PATH="D:\Fun\kaguya"
 
-sqlite_diagram.cmd %REPO_PATH%\kaguya_data.db %REPO_PATH%\schema.pdf
+..\..\_schemacrawler\schemacrawler.cmd --server=sqlite "--database=%REPO_PATH%\kaguya_data.db" --user=sa --password= --info-level=maximum -c=schema --output-format=png "-o=%REPO_PATH%\schema.png"
 
 ENDLOCAL
